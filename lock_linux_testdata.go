9fb6c8dd95f77fa6743b1e45ef49faed9b25fcce
Jack Hayhurst <jhayhurst@liquidweb.com>
2019-10-16 13:52:34 -0400

    wrote out lock literal data

---
+++ lock_linux_testdata.go
@@ -0,0 +1,31 @@
+package procinfo
+
+var Testdata_Lock = []Lock{
+	Lock{
+		Priority:  41,
+		ByteRange: false,
+		Exclusive: true,
+		PID:       5623,
+		DevMajor:  0x00,
+		DevMinor:  0x17,
+		Inode:     810,
+	},
+	Lock{
+		Priority:  42,
+		ByteRange: true,
+		Exclusive: false,
+		PID:       5705,
+		DevMajor:  0x00,
+		DevMinor:  0x17,
+		Inode:     1088,
+	},
+	Lock{
+		Priority:  61,
+		ByteRange: false,
+		Exclusive: true,
+		PID:       8819,
+		DevMajor:  0xfd,
+		DevMinor:  0x17,
+		Inode:     810,
+	},
+}
